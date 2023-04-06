7649b8821e954bd4d030fb55315c3afd2f879b1f
a <arawalarjun@gmail.com>
2023-04-06 11:27:08 -0700

    Initial Commit

---
+++ DisasterPreventer/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DisasterPreventer
+//
+//  Created by Arjun Rawal on 4/6/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        
+        Text("Disaster Preventer").font(.title)
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ DisasterPreventer/DisasterPreventerApp.swift
@@ -0,0 +1,17 @@
+//
+//  DisasterPreventerApp.swift
+//  DisasterPreventer
+//
+//  Created by Arjun Rawal on 4/6/23.
+//
+
+import SwiftUI
+
+@main
+struct DisasterPreventerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
